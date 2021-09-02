dd28971e1fbcd2ad1ab381c28e7fea1224e16387
mager <magerdima@gmail.com>
2021-09-02 18:42:25 +0300

    add message is

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -3,9 +3,7 @@ package controller;
 public class controller {
 
     public void controllermeth (String s) {
-
-
-
+        System.out.println("Message is:");
         System.out.println(s);
     }
 }
